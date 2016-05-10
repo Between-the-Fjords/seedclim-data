574c525ae1de4f3d342a3cebf8a14bbcbdfd6670
fja062 <francesca.jaroszynska@uib.no>
2016-05-10 13:29:07 +0200

    updated other data loading

--- seedclimComm/inst/importSource/loadOtherData.r
+++ seedclimComm/inst/importSource/loadOtherData.r
@@ -1,23 +1,22 @@
-require(RODBC)
 #load other data
 
 #traits
 traitsQ<-"SELECT taxon.*, [more traits].*, extraTraits.*
 FROM (taxon LEFT JOIN [more traits] ON taxon.species = [more traits].species) LEFT JOIN extraTraits ON taxon.species = extraTraits.speciescode
 ORDER BY taxon.species;"
-traits<-sqlQuery(con,traitsQ) 
-traits$species.1<-NULL
+traits<-dbGetQuery(con, traitsQ) 
+traits$species.1 <- NULL
 head(traits)
-names(traits)<-make.names(names(traits))
+names(traits) <- make.names(names(traits))
 traits[!is.na(traits$Flowering.start),c("F.Vår", "F.Fso", "F.Mso", "F.Sso", "F.Hø" )][is.na(traits[!is.na(traits$Flowering.start),c("F.Vår", "F.Fso", "F.Mso", "F.Sso", "F.Hø" )])]<-0
-traits[!is.na(traits$Lower),c("Nem", "BNem", "SBor", "MBor", "Nbor", "LAlp", "MAlp", "HAlp" )][is.na(traits[!is.na(traits$Lower),c("Nem", "BNem", "SBor", "MBor", "Nbor", "LAlp", "MAlp", "HAlp" )])]<-0
+traits[!is.na(traits$Lower),c("Nem", "BNem", "SBor", "MBor", "Nbor", "LAlp", "MAlp", "HAlp" )][is.na(traits[!is.na(traits$Lower),c("Nem", "BNem", "SBor", "MBor", "Nbor", "LAlp", "MAlp", "HAlp" )])] <- 0
 
 
-traits$alpine<-traits$Nem==0&traits$BNem==0&traits$SBor==0&traits$LAlp==1
-traits$lowland<-traits$HAlp==0&traits$MAlp==0&traits$LAlp==0&traits$Nem==1
+traits$alpine <- traits$Nem==0 & traits$BNem==0 & traits$SBor==0 & traits$LAlp==1
+traits$lowland <- traits$HAlp==0 & traits$MAlp==0 & traits$LAlp==0 & traits$Nem==1
 
 #fiter traits
-traits<-traits[traits$species%in%names(cover),]
+traits <- traits[traits$species %in% names(cover),]
 identical(as.character(traits$species), names(cover))
 
 #############################################################
@@ -27,7 +26,7 @@ turfenvQ<-"SELECT blocks.blockID, plots.plotID, turfs.turfID, turfs.TTtreat, blo
 FROM (plots AS plots_1 INNER JOIN blocks AS blocks_1 ON plots_1.blockID = blocks_1.blockID) INNER JOIN ((blocks INNER JOIN (plots INNER JOIN turfs ON plots.plotID = turfs.destinationPlotID) ON blocks.blockID = plots.blockID) INNER JOIN turfEnvironment ON turfs.turfID = turfEnvironment.turfID) ON plots_1.plotID = turfs.originPlotID 
 where not turfs.TTtreat ='' AND ((Not (turfEnvironment.Year)=2010));"
 
-turfenv<-sqlQuery(con,turfenvQ)
+turfenv<-dbGetQuery(con,turfenvQ)
 summary(turfenv)
 sort(rowSums(is.na(turfenv)))
 
