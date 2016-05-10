9cddb6a060ab94f3dc9b6b6dc9eedb07b38c2bbf
richadjtelford <richard.telford@bio.uib.no>
2016-05-11 00:24:07 +0200

    make connection split from load data

--- seedclimComm/R/load_community_data.r
+++ seedclimComm/R/load_community_data.r
@@ -1,12 +1,10 @@
 #' Imports Community data from source files
 #' 
-#' @param username is the username
-#' @param password is password
-#' @param host is host
-#' @param port is port
+#' @param con connection to database
 #' @param moss Load bryophyte data?
+#' @param closeCon Close connection after loading data
 #' @description Runs source files to load community data into memory. 
-#' @details Uses RODBC so only works with 32 bit version of windows.
+#' @details use make_connection() to get connection to database
 #' Currently loads
 #' \itemize{
 #'   \item{Cover data}
@@ -21,18 +19,11 @@
 #' \dontrun{load_community_data()}
 
 
-#' @export
-#' @importFrom RODBC odbcConnectAccess2007 odbcConnect
+#' @export 
+#' @importFrom BDI dbDisconnect
 
 
-load_community_data<-function(username = "", password = "", host = "127.0.0.1", port = 3306, moss = FALSE){
-  con <- dbConnect (RMySQL::MySQL(),
-                    username = username,
-                    password = password,
-                    host = host,
-                    port = port,
-                    dbname = "seedclimComm")
-  con<<-con#ugly hack otherwise not found in scope
+load_community_data<-function(con, closeCon = TRUE, moss = FALSE){
   source(file.path(system.file(package = "seedclimComm"), "importSource", "loadCover.r"))
   source(file.path(system.file(package = "seedclimComm"), "importSource", "loadSubplotfreq.r"))
   source(file.path(system.file(package = "seedclimComm"), "importSource", "loadOtherData.r"),encoding="UTF-8")
@@ -41,6 +32,8 @@ load_community_data<-function(username = "", password = "", host = "127.0.0.1", 
   }
   source(file.path(system.file(package = "seedclimComm"), "importSource", "seedclimColours.r"))
   
-  close(con)
+  if(closeCon){
+    dbDisconnect(con)
+  }
 
 }
