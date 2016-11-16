a4b9a03668fd10c8b16c286f44d2aa4c16011781
fja062 <francesca.jaroszynska@uib.no>
2016-11-16 18:29:48 +0100

    include new source of species corrections

--- seedclimComm/inst/uploadDataSource/dbConnection.R
+++ seedclimComm/inst/uploadDataSource/dbConnection.R
@@ -1,16 +1,13 @@
+library(DBI)
 con <- dbConnect(RMySQL::MySQL(), group = "seedclim")
-
 filelistall<-dir(path = "rawdata/", full.names = TRUE, pattern = "csv$")
 
-
-#import.data(c(filelist2009,filelist2010,filelist2011,filelist2012,filelist2013))
+source("inst/uploadDataSource/importcommunityNY_test_16.des.2013.r")
 import.data(c(filelistall), con)     #run me
-#import.data(c(filelist2011))
-#import.data(c(filelist2012))
-#import.data(c(filelist2013))
-import.data(c(filelist2010))           #run me ####warning NA by coercion
+
+source("inst/uploadDataSource/speciesCorrections.R")
 
 
-wipe()
 
+#wipe()
 close(con)
