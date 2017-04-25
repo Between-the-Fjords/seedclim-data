745fb0dfdde5b3b1a8b535890bb047aef21aa652
RagnhildGya <rgy042@student.uib.no>
2017-04-25 22:16:53 +0200

    Cleaning code, testing which model is the better one, and finding someway to plot a model on the plot

--- Traits/Mixed effect models.R
+++ Traits/Mixed effect models.R
@@ -18,39 +18,56 @@ AIC(model_SLA_1, model_SLA_0)
 qqnorm(resid(model_SLA_1))
 
 
+
+
 ## SLA ##
 
-SLA_model<- glmer(SLA ~scale(Temp)+scale(Precip)+scale(Temp):scale(Precip) + (1|Site), data= traitdata, fam=gaussian(link=log))
+#Two different models tryed out
 
-summary(SLA_model)
+#SLA_model<- glmer(SLA ~scale(Temp)+scale(Precip)+scale(Temp):scale(Precip) + (1|Site), data= traitdata, fam=gaussian(link=log))
 
+#SLA_model<-lmer(SLA ~ scale(Temp) + scale(Precip) + scale(Temp):scale(Precip) +  (1 | Site) + (1 | Species), data = traitdata)
 
-traitdata%>%
-  filter(Species %in% c("Agr_cap", "Ant_odo", "Cam_rot", "Des_ces", "Ver_off", "Ave_fle", "Luz_mul", "Bis_viv", "Pot_ere", "Alc_alp", "Tri_rep", "Tha_alp", "Ach_mil", "Nar_str", "Rum_ace"))%>%
-  ggplot(aes(x=Temp, y=SLA, color=T_level))+
-  geom_jitter(height=0)+
-  facet_wrap( ~ Species, ncol=5)+
-  geom_line(aes(y=MEM_SLA, group=1), size=0.8)
-  
-  
-  #geom_smooth(traitdata$MEM_SLA)
-  #geom_smooth(method="lme", aes(group=1), method.args=list(random=~1|Site))
+#summary(SLA_model)
 
 
-## Failed attempt at for looping the models ##
+## Making something to see which species I have collected from at most sites ##
+
+#hm<-traitdata%>%
+#  select(Species)%>%
+#  group_by(Species)%>%
+#  summarise(number=n())%>%
+#  arrange(-number)
+
+
 TheLucky15<-traitdata %>%
   filter(Species %in% c("Agr_cap", "Ant_odo", "Cam_rot", "Des_ces", "Ver_off", "Ave_fle", "Luz_mul", "Bis_viv", "Pot_ere", "Alc_alp", "Tri_rep", "Tha_alp", "Ach_mil", "Nar_str", "Rum_ace"))
 
+klm<-TheLucky15%>%
+  mutate(scale_Temp=scale(Temp))%>%
+  group_by(Species)%>%
+  do(augment(lmer(SLA ~scale_Temp + (1|Site), data= .)))
 
-SLA_model_Agr = NULL
+#scale(TheLucky15$Temp) #Used this to figure out which numbers to use to get the right temperature back from the scaled version
 
-for (i in TheLucky15$Species){
-  SLA_model_Agr[i] = lmer(SLA ~scale(Temp)+scale(Precip)+scale(Temp):scale(Precip)+(1|Site), data= TheLucky15, subset=Species==i)
-}
+
+klm<-klm%>%
+  mutate(Temp=scale_Temp*1.774107+8.433835) #Getting the real temperature back
+  
+TheLucky15%>%  
+  ggplot(aes(x=Temp, y=SLA, color=T_level))+
+  geom_jitter(height=0)+
+  facet_wrap( ~ Species, ncol=5)+
+  geom_line(aes(x=Temp, y=.fixed), data=klm, inherit.aes = FALSE)
+  
+
+
 
 
 ##Making all the models one by one ##
+library("broom")
 
+predict(SLA_model_Ant, interval="confidence", level=0.95)
 SLA_model_Ant<- lmer(SLA ~scale(Temp)+scale(Precip)+scale(Temp):scale(Precip) + (1|Site), data= traitdata, subset=Species=="Ant_odo")
 
 
@@ -67,10 +84,3 @@ SLA_model_Ver<- lmer(SLA ~scale(Temp)+scale(Precip)+scale(Temp):scale(Precip) + 
 traitdata$MEM_SLA <- predict(SLA_model, na.action = na.exclude)
 
 
-## Making something to see which species I have collected from at most sites ##
-
-hm<-traitdata%>%
-  select(Species)%>%
-  group_by(Species)%>%
-  summarise(number=n())%>%
-  arrange(-number)
