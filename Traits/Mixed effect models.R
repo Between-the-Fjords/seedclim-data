d453bd044a1ee1c49f45c0c5a654c055b491ce42
RagnhildGya <rgy042@student.uib.no>
2017-04-25 20:58:02 +0200

    Trying different ways to make the models and put them back into the dataframe so that I can plot the mixed effect models in the graps

--- Traits/Mixed effect models.R
+++ Traits/Mixed effect models.R
@@ -30,14 +30,44 @@ traitdata%>%
   ggplot(aes(x=Temp, y=SLA, color=T_level))+
   geom_jitter(height=0)+
   facet_wrap( ~ Species, ncol=5)+
-  geom_smooth(method="lme", aes(group=1), method.args=list(random=~1|Site))
+  geom_line(aes(y=MEM_SLA, group=1), size=0.8)
+  
+  
+  #geom_smooth(traitdata$MEM_SLA)
+  #geom_smooth(method="lme", aes(group=1), method.args=list(random=~1|Site))
+
+
+## Failed attempt at for looping the models ##
+TheLucky15<-traitdata %>%
+  filter(Species %in% c("Agr_cap", "Ant_odo", "Cam_rot", "Des_ces", "Ver_off", "Ave_fle", "Luz_mul", "Bis_viv", "Pot_ere", "Alc_alp", "Tri_rep", "Tha_alp", "Ach_mil", "Nar_str", "Rum_ace"))
+
+
+SLA_model_Agr = NULL
+
+for (i in TheLucky15$Species){
+  SLA_model_Agr[i] = lmer(SLA ~scale(Temp)+scale(Precip)+scale(Temp):scale(Precip)+(1|Site), data= TheLucky15, subset=Species==i)
+}
+
+
+##Making all the models one by one ##
+
+SLA_model_Ant<- lmer(SLA ~scale(Temp)+scale(Precip)+scale(Temp):scale(Precip) + (1|Site), data= traitdata, subset=Species=="Ant_odo")
+
+
+SLA_model_Cam<- lmer(SLA ~scale(Temp)+scale(Precip)+scale(Temp):scale(Precip) + (1|Site), data= traitdata, subset=Species=="Cam_rot")
+
+
+SLA_model_Des<- lmer(SLA ~scale(Temp)+scale(Precip)+scale(Temp):scale(Precip) + (1|Site), data= traitdata, subset=Species=="Des_ces")
+
+
+SLA_model_Ver<- lmer(SLA ~scale(Temp)+scale(Precip)+scale(Temp):scale(Precip) + (1|Site), data= traitdata, subset=Species=="Ver_off")
 
 
 
-SLA_model_Agr<- lmer(SLA ~scale(Temp)+scale(Precip)+scale(Temp):scale(Precip) + (1|Site), data= traitdata, subset=Species=="Agr_cap")
+traitdata$MEM_SLA <- predict(SLA_model, na.action = na.exclude)
 
-summary(SLA_model_Agr)
 
+## Making something to see which species I have collected from at most sites ##
 
 hm<-traitdata%>%
   select(Species)%>%
