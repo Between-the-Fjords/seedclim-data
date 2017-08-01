3666958ca2644bb383835c39e6e9d3db8133713d
Francesca Orinda Holl Jaroszynska <fja062@bio0221005.klientdrift.uib.no>
2017-08-01 11:44:56 +0200

    removing unused 'speciality' code

--- inst/graminoidRemovals/loadData.R
+++ inst/graminoidRemovals/loadData.R
@@ -298,42 +298,6 @@ colnames(timedeltacalc) <- paste0("delta", colnames(timedeltacalc))
 timedelta <- cbind((forbcom[forbcom$Year != 2011,]), timedeltacalc)
 
 
-
-################## FUNCTIONAL:: CALCULATING TRAITS DELTA ###################
-
-###### weighted means by specialism
-specialism <- my.GR.data %>%
-  group_by(ID, specialism) %>%
-  filter(functionalGroup != "graminoid") %>%
-  mutate(wmean_height = weighted.mean(height, cover), wmean_leafSize = weighted.mean(leafSize, cover), wmean_seedMass = weighted.mean(seedMass, cover), wmean_SLA = weighted.mean(SLA, cover), Year = Year, sumcover = sum(cover)) %>%
-  mutate(wmean_LDMC_local = weighted.mean(LDMC_mean, cover, na.rm = TRUE), wmean_SLA_local = weighted.mean(SLA_mean, cover, na.rm = TRUE), wmean_LTH_local = weighted.mean(Lth_mean, cover, na.rm = TRUE), wmean_LA_local = weighted.mean(LA_mean, cover, na.rm = TRUE), wmean_height_local = weighted.mean(Height_mean, cover, na.rm = TRUE)) %>%
-  distinct(ID, specialism, .keep_all = TRUE) %>%
-  select(-(functionalGroup:seedMass), -species) %>%
-  as.data.frame()
-
-specialism$specYear <- as.factor(paste(specialism$specialism, specialism$Year, sep = "_"))
-
-
-special <- sapply(1:nrow(specialism[specialism$TTtreat == "RTC",]), function(i){
-  R <- specialism[specialism$TTtreat == "RTC",][i,]
-  #browser()
-  cols <- c("wmean_height","wmean_SLA","wmean_leafSize", "wmean_seedMass", "sumcover", "wmean_LDMC_local", "wmean_SLA_local", "wmean_LTH_local", "wmean_LA_local", "wmean_height_local")
-  friend <- specialism$Year == R$Year & specialism$blockID == R$blockID & specialism$specialism == R$specialism & specialism$TTtreat == "TTC"
-  if(all (!friend)) {print(R$turfID)
-    return(rep(NA, length(cols)))}
-  stopifnot(sum(friend) == 1)
-  
-  f <- specialism[friend,]
-  x <- R[,cols] - f[,cols]
-  unlist(x)
-})
-special <- as.data.frame(t(special))
-colnames(special) <- paste0("delta", colnames(special))
-special <- cbind((specialism[specialism$TTtreat == "RTC",]), special)
-
-## ---- Explanatory.variable.deltas.end ---- 
-
-
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ###### SEEDLINGS ########
 
