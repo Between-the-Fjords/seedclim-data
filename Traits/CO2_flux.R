b65bbbaf14579f68053dd7c9dc3e53368f45f83a
RagnhildGya <rgy042@student.uib.no>
2017-09-04 13:41:49 +0200

    Cleaning up the code

--- Traits/CO2_flux.R
+++ Traits/CO2_flux.R
@@ -7,36 +7,19 @@ CO2_flux <- read.csv("Traits/data/CO2_GPP_1516Trait.csv", header=TRUE, sep=",")
 #str(CO2_flux)
 
 CO2_flux <- CO2_flux %>%
-  select(chamber, site, block, treatment, vegHeight.x, Reco15, GPP1200)%>%
-  rename(vegHeight= vegHeight.x) %>%
+  select(site, block, treatment, vegHeight=vegHeight.x, Reco15, GPP1200)%>%
   mutate(site = recode(site, ULV = "Ulv", ALR = "Alr", FAU = "Fau", LAV = "Lav", HOG = "Hog", VIK = "Vik", GUD = "Gud", RAM = "Ram", ARH = "Arh", SKJ = "Skj", VES = "Ves", OVS = "Ovs")) %>%
   mutate(turfID=paste0(site, block, treatment))
 
-CO2_traits_community <- full_join(short_wcommunity, CO2_flux, by=c("turfID"="turfID")) %>%
+CO2_traits_community <- full_join(wcommunity_df, CO2_flux, by=c("turfID"="turfID")) %>%
   mutate(site = factor(site, levels = c("Ulv", "Lav", "Gud", "Skj", "Alr", "Hog", "Ram", "Ves", "Fau", "Vik", "Arh", "Ovs")))%>%
   mutate(T_level = recode(site, Ulv = "Alpine", Lav = "Alpine",  Gud = "Alpine", Skj = "Alpine", Alr = "Sub-alpine", Hog = "Sub-alpine", Ram = "Sub-alpine", Ves = "Sub-alpine", Fau = "Boreal", Vik = "Boreal", Arh = "Boreal", Ovs = "Boreal"))
 
 #### Merging with biomass data ####
 
-CO2_mass_traits <- full_join(CO2_traits_community, biomass, by=c("turfID"="turfID"))
+CO2_mass_traits <- full_join(CO2_traits_community, distinct(biomass, turfID, total.biomass), by=c("turfID"="turfID"))
 
-CO2_mass_traits <- CO2_mass_traits%>%
-  select(-site.y)%>%
-  rename(site=site.x)
-
-## Making subsets of the temperature levels ##
-
-alp_com_CO2 <- CO2_mass_traits %>%
-  filter(T_level=="Alpine")%>%
-  select(Wmean_CN, Wmean_LDMC, Wmean_Lth, Wmean_LA, Wmean_SLA, Wmean_Height, cover, vegHeight, Reco15, GPP1200, total.biomass)
-
-subalp_com_CO2 <- CO2_mass_traits  %>%
-  filter(T_level=="Sub-alpine")%>%
-  select(Wmean_CN, Wmean_LDMC, Wmean_Lth, Wmean_LA, Wmean_SLA, Wmean_Height, cover, vegHeight, Reco15, GPP1200, total.biomass)
-
-bor_com_CO2 <- CO2_mass_traits %>%
-  filter(T_level=="Boreal")%>%
-  select(Wmean_CN, Wmean_LDMC, Wmean_Lth, Wmean_LA, Wmean_SLA, Wmean_Height, cover, vegHeight, Reco15, GPP1200, total.biomass)
+## Making subsets for comparison ##
 
 com_CO2 <- CO2_mass_traits %>%
   select(Wmean_CN, Wmean_LDMC, Wmean_Lth, Wmean_LA, Wmean_SLA, Wmean_Height, cover, vegHeight, Reco15, GPP1200, total.biomass)
@@ -93,16 +76,14 @@ model_Height <- lmer(GPP1200 ~ total.biomass + T_level + P_level + Wmean_Height 
 summary(model_Height)
 anova(model_Height)
 
-model_all_traits <- lmer(GPP1200 ~ total.biomass + T_level + P_level + Wmean_CN + Wmean_LDMC + Wmean_Lth + Wmean_LA + Wmean_SLA + Wmean_Height + (1|site/turfID), data=CO2_mass_traits)
+model_all_traits <- lmer(GPP1200 ~ total.biomass + T_level + P_level + Wmean_CN + scale(Wmean_LDMC) + scale(Wmean_Lth) + Wmean_LA + Wmean_SLA + Wmean_Height + (1|site/turfID), data=CO2_mass_traits)
 
 summary(model_all_traits)
 anova(model_all_traits)
 
 AIC(model_0, model_CN, model_LDMC, model_Lth, model_LA, model_SLA, model_Height, model_all_traits)
 
-#The best model is the one with Leaf thickness, and then leaf dry matter content, then carbon and nitrogen ratio and the leaf are. Specific leaf area and height was worse than the null model.
-#The CN ratio is the only trait that is close to being significant in the summary output of the model.
-#None of the traits have a significant effect from the anov output of the models.
+
 
 anova(model_0, model_CN)
 anova(model_0, model_LDMC)
@@ -115,9 +96,28 @@ anova(model_0, model_Height)
 
 ## Plotting the model to check for things ##
 
+par(mfrow=c(3,4))
+a <- plot(model_0)
+b <-plot(model_CN)
+plot(model_LDMC)
+plot(model_Lth)
+plot(model_LA)
+plot(model_SLA)
+plot(model_Height)
+par(mfrow=c(1,1))
+
+gridExtra::grid.arrange(a,b)
+
+qqnorm(residuals(model_0))
+qqline(resid(model_0))
+
+qqnorm(residuals(model_CN))
+qqline(resid(model_CN))
+
+
 model_CO2 <- CO2_mass_traits%>%
   filter(!is.na(total.biomass))
-  
+
 model_CO2$fit_0 <- predict(model_0)
 
 ggplot(model_CO2, aes(x=total.biomass, y=GPP1200))+
@@ -127,25 +127,7 @@ ggplot(model_CO2, aes(x=total.biomass, y=fit_0))+
   geom_point()
 
 
-par(mfrow=c(3,4))
-plot(model_0)
-plot(model_CN)
-plot(model_LDMC)
-plot(model_Lth)
-plot(model_LA)
-plot(model_SLA)
-plot(model_Height)
-par(mfrow=c(1,1))
-
-qqnorm(residuals(model_0))
-qqline(resid(model_0))
-
 #### Making graphs to visualize ####
-
-
-ggplot(CO2_traits_community, aes(x=vegHeight, y=GPP1200))+
-  geom_point()+
-  geom_smooth(method="gam", formula = y ~ s(x, bs = "cs"))
 
 ggplot(CO2_mass_traits, aes(x=total.biomass, y=GPP1200, col=T_level))+
   geom_point()
@@ -160,12 +142,17 @@ biomass%>%
   geom_col()+
   facet_wrap(~site)
 
+CO2_mass_traits%>%
+  ggplot(aes(x= site, y=Wmean_CN, col= T_level))+
+  geom_point()
 
-#### Models with several traits ####
+CO2_mass_traits%>%
+  ggplot(aes(x= site, y=GPP1200, col= T_level))+
+  geom_point()
 
-com_mod_1 <- lmer(GPP1200 ~ Wmean_Height*Wmean_LA*Wmean_SLA*Wmean_CN*Wmean_Lth*Wmean_LDMC + (1|Site/species), data=CO2_traits_community)
-
-com_mod_1 <- lmer(GPP1200 ~ Wmean_Height*Wmean_LA*Wmean_SLA*Wmean_CN*Wmean_Lth + (1|Site/species), data=CO2_traits_community)
-summary(com_mod_1)
+CO2_mass_traits%>%
+  ggplot(aes(x= Wmean_CN, y=GPP1200, col= T_level))+
+  geom_point()+
+  geom_smooth(aes(group=1), method="lm")
 
 
