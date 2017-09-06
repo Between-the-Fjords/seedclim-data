6ee3a1ef9c52e34832e5fd36d3c5198ab2623ecc
RagnhildGya <rgy042@student.uib.no>
2017-09-06 12:25:16 +0200

    Changing the T_level and P_level into actuall numbers

--- Traits/Cleaning.R
+++ Traits/Cleaning.R
@@ -14,10 +14,10 @@ traits <- traits %>%
   rename(Height=Height..mm., Lth_1=Lth.1..mm., Lth_2= Lth.2..mm., Lth_3= Lth.3..mm., Wet_mass=Wet.mass..g., Dry_mass=Dry.mass..g., Site=Location) %>%
   select(-Lth.average..mm.)%>%
   mutate(Date = mdy(Date))%>%
-  mutate(T_level = recode(Site, Ulv = "1", Lav = "1",  Gud = "1", Skj = "1", Alr = "2", Hog = "2", Ram = "2", Ves = "2", Fau = "3", Vik = "3", Arh = "3", Ovs = "3")) %>%
+  mutate(T_level = recode(Site, Ulv = "6.5", Lav = "6.5",  Gud = "6.5", Skj = "6.5", Alr = "8.5", Hog = "8.5", Ram = "8.5", Ves = "8.5", Fau = "10.5", Vik = "10.5", Arh = "10.5", Ovs = "10.5")) %>%
   mutate(Temp = recode(Site, Ulv=6.17, Lav=6.45, Gud=5.87, Skj=6.58, Alr=9.14, Hog=9.17, Ram=8.77, Ves=8.67, Fau=10.3, Vik=10.55, Arh=10.60, Ovs=10.78))%>%
   mutate(Precip= recode(Site, Ulv=596, Lav=1321, Gud=1925, Skj=2725, Alr=789, Hog=1356, Ram=1848, Ves=3029, Fau=600, Vik=1161, Arh=2044, Ovs=2923))%>%
-  mutate(P_level = recode(Site, Ulv = "1", Alr = "1", Fau = "1", Lav = "2", Hog = "2", Vik = "2", Gud = "3", Ram = "3", Arh = "3", Skj = "4", Ves = "4", Ovs = "4")) %>%
+  mutate(P_level = recode(Site, Ulv = "600", Alr = "600", Fau = "600", Lav = "1200", Hog = "1200", Vik = "1200", Gud = "2000", Ram = "2000", Arh = "2000", Skj = "2700", Ves = "2700", Ovs = "2700")) %>%
   mutate(LDMC=Dry_mass/Wet_mass)%>%
   mutate(Site = factor(Site, levels = c("Ulv", "Lav", "Gud", "Skj", "Alr", "Hog", "Ram", "Ves", "Fau", "Vik", "Arh", "Ovs"))) %>%
   mutate(Lth_ave=rowMeans(select(traits, matches("^Lth\\.\\d")), na.rm = TRUE)) %>%
