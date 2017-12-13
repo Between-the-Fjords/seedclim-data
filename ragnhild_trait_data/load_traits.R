361ff2f1268fb22393765325dcdffe06883e59b7
Francesca Orinda Holl Jaroszynska <fja062@bio0221005.klientdrift.uib.no>
2017-12-13 12:19:07 +0100

    filling in missing local traits with global ones

--- ragnhild_trait_data/load_traits.R
+++ ragnhild_trait_data/load_traits.R
@@ -1,6 +1,5 @@
 #### Libraries ####
 library(lubridate)
-library(ggplot2)
 library(stringr)
 
 #########################################################################
@@ -112,3 +111,24 @@ traitdata <- traits %>%
 #  mutate(SLA_mean = plyr::mapvalues(SLA_mean, from = "Inf", to = "NA")) %>%
 #  mutate(SLA_mean_global = plyr::mapvalues(SLA_mean_global, from = "Inf", to = "NA"))
 
+traitdata <- traitdata %>% 
+  group_by(siteID, species) %>% 
+  mutate(CN_mean = if_else(is.na(CN_mean),
+                            CN_mean_global,
+                            CN_mean)) %>% 
+  mutate(SLA_mean = if_else(is.na(SLA_mean),
+                           SLA_mean_global,
+                           SLA_mean)) %>% 
+  mutate(Lth_mean = if_else(is.na(Lth_mean),
+                           Lth_mean_global,
+                           Lth_mean)) %>% 
+  mutate(LA_mean = if_else(is.na(LA_mean),
+                           LA_mean_global,
+                           LA_mean)) %>% 
+  mutate(Height_mean = if_else(is.na(Height_mean),
+                          Height_mean_global,
+                          Height_mean)) %>% 
+  mutate(LDMC_mean = if_else(is.na(LDMC_mean),
+                             LDMC_mean_global,
+                             LDMC_mean))
+  
