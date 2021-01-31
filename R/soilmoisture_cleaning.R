863ba2410d5f8e628a14a5b51c2e003c277ec01d
Joseph Gaudard <jga051@bio3103914.klientdrift.uib.no>
2021-01-31 18:09:41 +0100

    did a graph

--- R/soilmoisture_cleaning.R
+++ R/soilmoisture_cleaning.R
@@ -1,8 +1,16 @@
 # This script is to clean the soil moisture point measurements
 library("dataDownloader")
 library(tidyverse)
-# import the data from OSF
+# import the data from OSF and read data
 
 
+soilmoisture_raw <- read_csv("SeedClim_Plot_SoilMoisture.csv", col_types = "ffDffd") %>% 
+  rename(
+    "ID" = "X1" # unique ID for each measurement
+  ) 
 
-# graph soil moisture vs date with site as fill
+# graph soil moisture vs date with site as fill and plotID as point label
+
+ggplot(soilmoisture_raw, aes(x = date, y = soil_moisture, fill = site, colour = site)) +
+  # geom_bar()
+  geom_point()
