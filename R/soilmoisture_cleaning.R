513508fb2299f9af209accbe33b42d54833f4770
Joseph Gaudard <jga051@bio3103914.klientdrift.uib.no>
2021-01-31 17:33:16 +0100

    first commit

---
+++ R/soilmoisture_cleaning.R
@@ -0,0 +1,8 @@
+# This script is to clean the soil moisture point measurements
+library("dataDownloader")
+library(tidyverse)
+# import the data from OSF
+
+
+
+# graph soil moisture vs date with site as fill
